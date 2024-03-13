66aac7dc018da07568b22d9bca0df4fbe7c79c77
Vincenzolaveglia <vincenzolaveglia10@gmail.com>
2024-03-13 16:23:01 +0100

    Modifiche giorno 3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using Gestionale_Pizzeria.Models;
+using static Gestionale_Pizzeria.Models.Carrello;
 
 namespace Gestionale_Pizzeria.Controllers
 {
@@ -37,6 +39,92 @@ namespace Gestionale_Pizzeria.Controllers
                 return HttpNotFound();
             }
             return View(prodotto);
+        }
+
+        public ActionResult VisualizzaCarrello()
+        {
+            if (Session["Carrello"] is List<CarrelloItem> carrelloItems && carrelloItems.Any())
+            {
+                return View(carrelloItems);
+            }
+            else
+            {
+                return View("Home");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AggiungiAlCarrello(int id, string nome, decimal prezzo, int quantita)
+        {
+            if (!(Session["Carrello"] is List<CarrelloItem> carrello))
+            {
+                carrello = new List<CarrelloItem>();
+            }
+
+            var carrelloItem = carrello.FirstOrDefault(item => item.Prodotto.IdProdotti == id);
+
+            if (carrelloItem != null)
+            {
+                carrelloItem.Quantita += quantita;
+            }
+            else
+            {
+                carrello.Add(new CarrelloItem
+                {
+                    Prodotto = new Prodotti { IdProdotti = id, Nome = nome, Prezzo = prezzo },
+                    Quantita = quantita
+                });
+            }
+
+            Session["Carrello"] = carrello;
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult ConcludiOrdine(string indirizzoConsegna, string noteSpeciali)
+        {
+
+            int userId = (int)Session["UserId"];
+
+            if (userId == 0)
+            {
+                FormsAuthentication.SignOut();
+            }
+
+            if (Session["UserId"] != null)
+            {
+                if (Session["Carrello"] is List<CarrelloItem> carrelloItems && carrelloItems.Any())
+                {
+                    decimal totale = carrelloItems.Sum(item => item.Prodotto.Prezzo * item.Quantita);
+
+                    var nuovoOrdine = new Ordini
+                    {
+                        UtenteId = (int)Session["UserId"],
+                        DataOrdine = DateTime.Now,
+                        IsCompleto = "false",
+                        IndirizzoConsegna = indirizzoConsegna,
+                        Nota= noteSpeciali,
+                        OrdiniProdotti = carrelloItems.Select(item => new OrdiniProdotti
+                        {
+                            ProdottoId = item.Prodotto.IdProdotti,
+                            Quantita = item.Quantita
+                        }).ToList()
+                    };
+
+                    db.Ordini.Add(nuovoOrdine);
+                    db.SaveChanges();
+
+                    Session["Carrello"] = null;
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return RedirectToAction("Login");
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Controllers/OrdineController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Gestionale_Pizzeria.Models;
+
+namespace Gestionale_Pizzeria.Controllers
+{
+    [Authorize]
+    public class OrdineController : Controller
+    {
+        readonly DBContext db = new DBContext();
+
+        public ActionResult ListaOrdine()
+        {
+            int? userId = (int?)Session["UserId"];
+
+            if (userId == 0)
+            {
+                FormsAuthentication.SignOut();
+                return RedirectToAction("Login");
+            }
+
+            if (userId.HasValue)
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    var ordineAdmin = db.Ordini.Include(o => o.Utenti);
+                    return View(ordineAdmin.ToList());
+                }
+                else
+                {
+                    var ordineUtente = db.Ordini.Include(o => o.Utenti).Where(o => o.UtenteId == userId.Value).ToList();
+                    return View(ordineUtente);
+                }
+            }
+            return View(new List<Ordini>());
+        }
+
+        public ActionResult ModificaOrdine(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ordini ordine = db.Ordini.Find(id);
+            if (ordine == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdUtente = new SelectList(db.Utenti, "IdUtente", "Nome", ordine.UtenteId);
+            return View(ordine);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ModificaOrdine(Ordini ordine)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ordine).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ListaOrdine");
+            }
+            ViewBag.IdUtente = new SelectList(db.Utenti, "IdUtente", "Nome", ordine.UtenteId);
+            return View(ordine);
+        }
+
+        public ActionResult EliminaOrdine(int id)
+        {
+            Ordini ordine = db.Ordini.Find(id);
+            db.Ordini.Remove(ordine);
+            db.SaveChanges();
+            return RedirectToAction("ListaOrdine");
+        }
+
+
+        public ActionResult ControlloOrdine()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult OrdiniEvasi()
+        {
+            {
+                DateTime today = DateTime.Today;
+                DateTime tomorrow = today.AddDays(1);
+
+                List<Ordini> ordine = db.Ordini.Where(a => a.IsCompleto == "evaso" && a.DataOrdine >= today && a.DataOrdine < tomorrow).ToList();
+                List<OrdineEvaso> ordineEvaso = new List<OrdineEvaso>();
+
+                foreach (var o in ordine)
+                {
+                    ordineEvaso.Add(new OrdineEvaso
+                    {
+                        Id = o.UtenteId,
+                        Nome = o.Utenti.Username,
+                        TotaleOrdiniOggi = ordine.Count()
+                    });
+                }
+
+                return Json(ordineEvaso);
+            }
+        }
+
+    }
+}
